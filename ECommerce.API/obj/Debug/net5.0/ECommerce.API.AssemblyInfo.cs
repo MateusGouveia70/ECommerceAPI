3218146c652aa3df2ec3d684747e13f3c059b932
MateusGouveia70 <mateusgouveiadev@gmail.com>
2022-02-28 03:30:35 -0300

    EndPoints testados

--- ECommerce.API/obj/Debug/net5.0/ECommerce.API.AssemblyInfo.cs
+++ ECommerce.API/obj/Debug/net5.0/ECommerce.API.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.API")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
